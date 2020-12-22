3ed25944c7fb29d42a714ce0fb7b634d043cbfa1
Arthur Silva <arxdsilva@gmail.com>
2020-12-22 11:24:05 -0300

    add removeCommonWords

--- search.go
+++ search.go
@@ -19,3 +19,40 @@ func toLower(txtSet []string) (lc []string) {
 	}
 	return lc
 }
+
+// 100 common words in eng
+// we'll drop it to prevent getting too much unwanted
+// https://www.espressoenglish.net/the-100-most-common-words-in-english/
+var commonWords = map[string]string{
+	"the": "", "at": "", "there": "", "some": "", "my": "",
+	"of": "", "be": "", "use": "", "her": "", "than": "",
+	"and": "", "this": "", "an": "", "would": "", "first": "",
+	"a": "", "have": "", "each": "", "make": "", "water": "",
+	"to": "", "from": "", "which": "", "like": "", "been": "",
+	"in": "", "or": "", "she": "", "him": "", "call": "",
+	"is": "", "one": "", "do": "", "into": "", "who": "",
+	"you": "", "had": "", "how": "", "time": "", "oil": "",
+	"that": "", "by": "", "their": "", "has": "", "its": "",
+	"it": "", "word": "", "if": "", "look": "", "now": "",
+	"he": "", "but": "", "will": "", "two": "", "find": "",
+	"was": "", "not": "", "up": "", "more": "", "long": "",
+	"for": "", "what": "", "other": "", "write": "", "down": "",
+	"on": "", "all": "", "about": "", "go": "", "day": "",
+	"are": "", "were": "", "out": "", "see": "", "did": "",
+	"as": "", "we": "", "many": "", "number": "", "get": "",
+	"with": "", "when": "", "then": "", "no": "", "come": "",
+	"his": "", "your": "", "them": "", "way": "", "made": "",
+	"they": "", "can": "", "these": "", "could": "", "may": "",
+	"I": "", "said": "", "so": "", "people": "", "part": ""}
+
+// removeCommonWords helps the search to be more meaningful
+// avoiding common words used in eng
+func removeCommonWords(txtSet []string) (rdcw []string) {
+	rdcw = make([]string, 0, len(txtSet))
+	for _, word := range txtSet {
+		if _, ok := commonWords[word]; !ok {
+			rdcw = append(rdcw, word)
+		}
+	}
+	return
+}
--- search_test.go
+++ search_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,4 +27,18 @@ func Test_toLower(t *testing.T) {
 	}
 	lower := toLower(splitTxt)
 	assert.Equal(t, "the", lower[0])
+}
+
+func Test_removeCommonWords(t *testing.T) {
+	txt := "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab"
+	splitTxt := splitter(txt)
+	assert.Equal(t, 40, len(splitTxt))
+	for _, tx := range splitTxt {
+		assert.NotContains(t, tx, ".")
+		assert.NotContains(t, tx, ",")
+	}
+	lower := toLower(splitTxt)
+	fmt.Println(lower, len(lower))
+	filtered := removeCommonWords(lower)
+	assert.Equal(t, 34, len(filtered))
 }
