fe4b152644a88e5bd3bc9339179244c1185438de
Arthur Silva <arxdsilva@gmail.com>
2020-12-22 14:52:59 -0300

    integrate filterText to search

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -37,7 +38,7 @@ func main() {
 
 type Searcher struct {
 	CompleteWorks string
-	SuffixArray   *suffixarray.Index
+	Index         *suffixarray.Index
 }
 
 func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
@@ -67,16 +68,18 @@ func (s *Searcher) Load(filename string) error {
 	if err != nil {
 		return fmt.Errorf("Load: %w", err)
 	}
-	s.CompleteWorks = string(dat)
-	s.SuffixArray = suffixarray.New(dat)
+	s.CompleteWorks = strings.ToLower(string(dat))
+	s.Index = suffixarray.New(dat)
 	return nil
 }
 
-func (s *Searcher) Search(query string) []string {
-	idxs := s.SuffixArray.Lookup([]byte(query), -1)
-	results := []string{}
-	for _, idx := range idxs {
-		results = append(results, s.CompleteWorks[idx-250:idx+250])
+func (s *Searcher) Search(query string) (results []string) {
+	qSplitted := filterText(query)
+	for _, word := range qSplitted {
+		idxs := s.Index.Lookup([]byte(word), -1)
+		for _, idx := range idxs {
+			results = append(results, s.CompleteWorks[idx-250:idx+250])
+		}
 	}
 	return results
 }
